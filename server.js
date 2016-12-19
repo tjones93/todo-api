42552d6dcd83ed8ca137717aefdcae64f40cad59
Taylor Jones <taylor@joneses.net>
2016-12-18 21:10:35 -0500

    more incremental changes

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+
+var port = process.env.port || 3000;
+
+app.get("/", function(req, res){
+    res.send("Todo API Root");
+});
+
+app.listen(port, function(){
+    console.log("Express listening on port: " + port + "!");
+})
+
