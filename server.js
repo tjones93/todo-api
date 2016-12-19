0bb96a686dd5bfedb1d1b28600ac5418be1b1fb6
Taylor Jones <taylor@joneses.net>
2016-12-18 23:29:11 -0500

    added todos

--- server.js
+++ server.js
@@ -3,11 +3,33 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
-app.get("/", function(req, res){
-    res.send("HELL YEAH!!!");
+var ToDos = [{
+    id: 1,
+    description: "Meet mom for lunch",
+    completed: false
+}, {
+    id: 2,
+    description: "Go to Market",
+    completed: false
+}, {
+    id:3, 
+    description: "Watch TV",
+    completed: true
+}];
+
+app.get("/", function (req, res) {
+    res.send("Todo API Root");
 });
+
+//GET /todos 
+app.get("/todos", function(req,res){
+    res.json(ToDos);
+});
+//get /todo/id
+//app.get("/todo/:id", function(req,res){
+//    res.json(ToDos(i));
+//})
 
 app.listen(port, function () {
-	console.log('Express server started on port ' + port + '!');
+    console.log('Express server started on port ' + port + '!');
 });
-
