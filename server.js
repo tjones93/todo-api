e8ea90c72a5089699921375d141a5293c1e3d1d4
Taylor Jones <taylor@joneses.net>
2016-12-18 23:11:25 -0500

    updated message

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get("/", function(req, res){
-    res.send("ISNT THIS COOL!?!?!?!?");
+    res.send("HELL YEAH!!!");
 });
 
 app.listen(port, function () {
